a7e71bb99ada8722428687f1a0114749294e8baf
deanwebbdeveloper <dean@deanwebbdeveloper.com>
2018-04-22 22:39:22 +0100

    Started Chapter 5
    Chapter 5 - 5.2 & 5.3 (Started, to be finished)

---
+++ 5 - Learning To Program - Branch and Loop/5.2 - If The Shoe Fits.js
@@ -0,0 +1,12 @@
+var getDieRoll = function(dieSize) {
+    var result = Math.ceil(dieSize * Math.random());
+    return result;
+}
+
+var roll = getDieRoll(6);
+
+if (roll >= 5) {
+  console.log("Great roll");
+}
+
+console.log("You rolled a " + roll);
