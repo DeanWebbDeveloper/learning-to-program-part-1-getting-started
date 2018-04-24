a94f7ba6247058957eaa065396e690669969dcb4
deanwebbdeveloper <dean@deanwebbdeveloper.com>
2018-04-24 18:08:14 +0100

    Chapter 8 - Final
    Chapter 8 completed
    
    Course completed

---
+++ 8 - Learning To Program - Final/8.3 - ExpressJS/gradeBook/program.js
@@ -0,0 +1,10 @@
+var book = require("./lib/grades").book;
+var express = require("express");
+var app = express();
+
+app.get("/", function(req,res) {
+	res.send("Hello,World!");
+});
+
+app.listen(3000);
+console.log("Server ready...");
